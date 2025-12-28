2fb2dfeb381b6259af2fb0063c38cb8e67fcd600
ngovantinh-ops-pwn <ngovantinh27092005@gmail.com>
2025-12-28 10:34:03 +0700

    Lab4-6

---
+++ Exercise_Lab04-Lab06/Cau4/Menu.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Cau4
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Server = new System.Windows.Forms.Button();
+            this.btn_Client = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Server
+            // 
+            this.btn_Server.Font = new System.Drawing.Font("Cascadia Code SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Server.Location = new System.Drawing.Point(278, 86);
+            this.btn_Server.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Server.Name = "btn_Server";
+            this.btn_Server.Size = new System.Drawing.Size(133, 40);
+            this.btn_Server.TabIndex = 0;
+            this.btn_Server.Text = "Server";
+            this.btn_Server.UseVisualStyleBackColor = true;
+            this.btn_Server.Click += new System.EventHandler(this.btn_Server_Click);
+            // 
+            // btn_Client
+            // 
+            this.btn_Client.Font = new System.Drawing.Font("Cascadia Code SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Client.Location = new System.Drawing.Point(244, 182);
+            this.btn_Client.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Client.Name = "btn_Client";
+            this.btn_Client.Size = new System.Drawing.Size(204, 40);
+            this.btn_Client.TabIndex = 1;
+            this.btn_Client.Text = "Client";
+            this.btn_Client.UseVisualStyleBackColor = true;
+            this.btn_Client.Click += new System.EventHandler(this.btn_Client_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.btn_Client);
+            this.Controls.Add(this.btn_Server);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Server;
+        private System.Windows.Forms.Button btn_Client;
+    }
+}
